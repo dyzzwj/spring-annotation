5b1d87c6486e20298401a20decb9abb3360c22b7
dyzzwj <dyzwj1998@163.com>
2020-07-13 19:10:13 +0800

    07-13

--- src/test/java/IOCTest_AOP.java
+++ src/test/java/IOCTest_AOP.java
@@ -3,6 +3,11 @@ import com.dyzwj.config.ConfigurationOfAop;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * @author zhengwenjie
  * @version 1.0.0
@@ -18,5 +23,51 @@ public class IOCTest_AOP {
         MathCaculate mathCaculate = applicationContext.getBean(MathCaculate.class);
         System.out.println(mathCaculate);
         mathCaculate.div(1,1);
+
+    }
+
+    @Test
+    public void test2(){
+
+        List<Integer> lists = new ArrayList<Integer>();
+        lists.add(1);
+        lists.add(2);
+        lists.add(3);
+        lists.add(4);
+        lists.add(5);
+        int size = lists.size();
+        for (int i = 0; i < lists.size(); i++) {
+            if(3==lists.get(i) || 4 == lists.get(i)){
+                lists.remove(i); //[1, 2, 4, 5]
+//                lists.remove(lists.get(i));//[1, 2, 4, 5]
+            }
+        }
+
+        System.out.println(lists);
+    }
+
+
+    @Test
+    public  void test3(){
+
+        List<Integer> lists = new ArrayList<Integer>();
+        lists.add(1);
+        lists.add(2);
+        lists.add(3);
+        lists.add(4);
+        lists.add(5);
+        Iterator<Integer> iterator = lists.iterator();
+        while (iterator.hasNext()){
+            int next = iterator.next();
+            if(next == 3 || next == 4){
+                iterator.remove();
+            }
+            System.out.println(lists.size());
+        }
+        System.out.println(lists);
+
+        Map<Object, Object> hashMap = new ConcurrentHashMap<>();
+
+
     }
 }
